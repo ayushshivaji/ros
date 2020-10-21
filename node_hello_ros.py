4231eef68c09c41a2e15e487ab9aea51de409d15
kira <71763446+ayushclashroyale@users.noreply.github.com>
2020-10-21 12:04:27 +0530

    Update node_hello_ros.py

--- node_hello_ros.py
+++ node_hello_ros.py
@@ -4,13 +4,13 @@ import rospy
 
 
 def main():
-	rospy.init_node('node_hello_ros', anonymous=True)
+	rospy.init_node('node_hello_ros', anonymous=True)#initializing the node
 
-	rospy.loginfo("Hello World")
+	rospy.loginfo("Hello World")#print the message
 
-	rospy.spin()
+	rospy.spin()#doesn't stop the node until interrupt is recieved
 
-if __name__ == '__main__':
+if __name__ == '__main__':#main def
 	try:
 		main()
 	except rospy.ROSInterruptException:
